d2637a6f991b4f9cbe87404500b254ccfa439ca9
frankwon11 <frankwon11@gmail.com>
2024-08-11 01:26:23 +0900

    [Feat/#6] 마커 도로명 주소 받아오기 완료

--- Chiara/Views/Maps/StreetDrainSheetView.swift
+++ Chiara/Views/Maps/StreetDrainSheetView.swift
@@ -28,45 +28,47 @@ struct StreetDrainSheetView: View {
 //    }
     
     var body: some View {
-        VStack(alignment: .center) {
-            HStack {
-                Text("\(locationManager.currentPlace)")
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                    .lineLimit(nil)
-                    .multilineTextAlignment(.leading)
+        ScrollView {
+            VStack(alignment: .center) {
+                HStack {
+                    Text("\(locationManager.currentPlace)")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .lineLimit(nil)
+                        .multilineTextAlignment(.leading)
+                    Spacer()
+                }
+                
+                // TODO: 이미지 추가 예정
+                Rectangle()
+                    .frame(width: 300, height: 200)
+                
                 Spacer()
+                
+                NavigationLink {
+                    // TODO: 이동 동작 추가
+                } label: {
+                    Text("Clean It Up")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.black)
+                        .padding(.horizontal, 25)
+                        .padding(.vertical, 20)
+                        .background {
+                            RoundedRectangle(cornerRadius: 30)
+                                .fill(.accent)
+                        }
+                }
+                .padding(.bottom, 20)
             }
-            
-            // TODO: 이미지 추가 예정
-            Rectangle()
-                .frame(width: 300, height: 200)
-            
-            Spacer()
-            
-            NavigationLink {
-                // TODO: 이동 동작 추가
-            } label: {
-                Text("Clean It Up")
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                    .foregroundStyle(.black)
-                    .padding(.horizontal, 25)
-                    .padding(.vertical, 20)
-                    .background {
-                        RoundedRectangle(cornerRadius: 30)
-                            .fill(.accent)
-                    }
-            }
-            .padding(.bottom, 20)
-        }
-        .padding(.horizontal, 20)
-        .padding(.top, 38)
-        .navigationBarBackButtonHidden()
-        .onAppear {
-            if let streetDrain = streetDrain {
-                let location = CLLocation(latitude: streetDrain.latitude, longitude: streetDrain.longitude)
-                locationManager.convertLocationToAddress(location: location)
+            .padding(.horizontal, 20)
+            .padding(.top, 38)
+            .navigationBarBackButtonHidden()
+            .onAppear {
+                if let streetDrain = streetDrain {
+                    let location = CLLocation(latitude: streetDrain.latitude, longitude: streetDrain.longitude)
+                    locationManager.convertLocationToAddress(location: location)
+                }
             }
         }
     }
