e820eb98681b8bf61b7241281ccf3e2c646f0063
Lee Sihyeong <poeticform@10-141-55-243.0200.pos.kr.iosda.org>
2024-08-11 00:52:53 +0900

    [Feat/#7-1] 커밋 내용 피알에 붙여넣을게요

---
+++ Chiara/Views/CameraView/CameraView.swift
@@ -0,0 +1,80 @@
+//
+//  CameraView.swift
+//  Chiara
+//
+//  Created by Lee Sihyeong on 8/11/24.
+//
+
+import SwiftUI
+
+struct CameraView: View {
+
+    @ObservedObject var cameraViewModel: CameraViewModel
+    @State var isShuttered: Bool = false
+
+    var body: some View {
+        NavigationStack {
+            ZStack(alignment: .bottom) {
+                if !isShuttered {
+                    cameraViewModel.cameraPreview
+                        .onAppear {
+                            cameraViewModel.configure()
+                        }
+                        .padding(.horizontal, 20)
+                        .padding(.vertical, 135)
+
+                    Button {
+                        cameraViewModel.capturePhoto()
+                        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) {
+                            isShuttered = true
+                        }
+                    } label: {
+                        ZStack {
+                            Circle()
+                                .frame(width: 67, height: 67)
+                            // TODO: Color change
+                                .foregroundStyle(.blue)
+                            Circle()
+                                .frame(width: 61, height: 61)
+                                .foregroundStyle(.black)
+                            Circle()
+                                .frame(width: 53, height: 53)
+                                .foregroundStyle(.white)
+                        }
+                    }
+                    .padding(.bottom, 135 + 16)
+                } else {
+                    if let recentImage = cameraViewModel.model.recentImage {
+                        Image(uiImage: recentImage)
+                            .resizable()
+                            .clipShape(RoundedRectangle(cornerRadius: 40))
+                            .padding(.horizontal, 20)
+                            .padding(.vertical, 135)
+                    }
+
+                    Button {
+                        isShuttered = false
+                    } label: {
+                        ZStack {
+                            Circle()
+                                .frame(width: 67, height: 67)
+                                // TODO: Color change
+                                .foregroundStyle(.blue)
+                            Circle()
+                                .frame(width: 61, height: 61)
+                                .foregroundStyle(.black)
+                            Circle()
+                                .frame(width: 53, height: 53)
+                                .foregroundStyle(.white)
+                            Image(systemName: "arrow.circlepath")
+                                .foregroundStyle(.black)
+                        }
+                    }
+                    .padding(.bottom, 135 + 16)
+                }
+            }
+            .navigationTitle("Upload")
+            .ignoresSafeArea()
+        }
+    }
+}
---
+++ Chiara/Views/CameraView/CameraViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  CameraViewModel.swift
+//  Chiara
+//
+//  Created by Lee Sihyeong on 8/11/24.
+//
+
+import AVFoundation
+import SwiftUI
+
+class CameraViewModel: ObservableObject {
+    let model: Camera
+    let session: AVCaptureSession
+    let cameraPreview: AnyView
+
+    init() {
+        model = Camera()
+        session = model.session
+        cameraPreview = AnyView(CameraPreviewView(session: session))
+    }
+
+    @Published var isFlashOn = false
+    @Published var isSilentModeOn = false
+
+    func configure() {
+        model.requestAndCheckPermissions()
+    }
+
+    func capturePhoto() {
+        model.capturePhoto()
+    }
+}
