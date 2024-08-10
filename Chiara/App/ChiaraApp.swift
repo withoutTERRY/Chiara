5e989f5a65e899737fe7b0bce70ac03efc7e22d3
frankwon11 <frankwon11@gmail.com>
2024-08-10 20:13:57 +0900

    [Design/#6] StreetDrainMapPin 이미지 추가

--- Chiara/App/ChiaraApp.swift
+++ Chiara/App/ChiaraApp.swift
@@ -14,9 +14,11 @@ struct ChiaraApp: App {
     @StateObject private var locationManager = LocationManager()
     @StateObject private var streetDrainManager = StreetDrainManager.shared
     
+    @State private var streetDrainList: [StreetDrain] = [StreetDrain(id: UUID().uuidString,
+                                                                     address: "77, Cheongam-ro, Nam-gu, Pohang-si, Gyeongsangbuk-do, Republic of Korea", latitude: 36.01403209698482, longitude: 129.32589456302887, trashType: .cigarette, isCleaned: false)]
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MapView(streetDrainList: $streetDrainList)
                 .environmentObject(locationManager)
                 .environmentObject(streetDrainManager)
                 .onAppear {
