cc260a987a44e5787f46ec80c7214ea21d7097fa
frankwon11 <frankwon11@gmail.com>
2024-08-10 17:58:15 +0900

    [Feat/#4] App에 LocationManager 연결

--- Chiara/App/ChiaraApp.swift
+++ Chiara/App/ChiaraApp.swift
@@ -9,9 +9,16 @@ import SwiftUI
 
 @main
 struct ChiaraApp: App {
+    @StateObject private var locationManager = LocationManager()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(locationManager)
+                .onAppear {
+                    // 위치 정보 접근 권한 요청을 합니다.
+                    locationManager.requestLocation()
+                }
         }
     }
 }
