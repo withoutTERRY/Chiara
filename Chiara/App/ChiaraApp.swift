ce09d0f27ba915b855859ab9677a8eabcfcaaf64
eomchanu <djacksdn1@icloud.com>
2024-08-10 16:58:17 +0900

    [Chore] 프로젝트 생성 및 초기 설정

---
+++ Chiara/App/ChiaraApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChiaraApp.swift
+//  Chiara
+//
+//  Created by Eom Chanwoo on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ChiaraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
