abcfc7c20f99da9b74c18fd4c2e8259be4cec269
frankwon11 <frankwon11@gmail.com>
2024-08-11 01:47:14 +0900

    [Feat/#6] NavigationStack 정의

--- Chiara/App/ChiaraApp.swift
+++ Chiara/App/ChiaraApp.swift
@@ -14,18 +14,39 @@ struct ChiaraApp: App {
     @StateObject private var locationManager = LocationManager()
     @StateObject private var streetDrainManager = StreetDrainManager.shared
     
+    @State private var isLoading: Bool = false
+    @State private var showSplashView: Bool = false
+    
     var body: some Scene {
         WindowGroup {
-            MapView()
-                .environmentObject(locationManager)
-                .environmentObject(streetDrainManager)
-                .onAppear {
-                    // 위치 정보 접근 권한 요청을 합니다.
-                    locationManager.requestLocation()
-                    
-                    // 실시간 배수구 정보를 받아옵니다.
-                    streetDrainManager.listenToRealtimeDatabase()
+            NavigationStack {
+                if showSplashView {
+                    SplashView()
+                        .onAppear {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                                withAnimation {
+                                    showSplashView = false
+                                }
+                            }
+                        }
+                } else {
+                    if isLoading {
+                        ProgressView("loading data...")
+                            .progressViewStyle(DefaultProgressViewStyle())
+                    } else {
+                        MapView()
+                    }
                 }
+            }
+            .onAppear {
+                // 위치 정보 접근 권한 요청을 합니다.
+                locationManager.requestLocation()
+                
+                // 실시간 배수구 정보를 받아옵니다.
+                streetDrainManager.listenToRealtimeDatabase()
+            }
+            .environmentObject(locationManager)
+            .environmentObject(streetDrainManager)
         }
     }
 }
---
+++ Chiara/Views/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  Chiara
+//
+//  Created by sseungwonnn on 8/11/24.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    SplashView()
+}
